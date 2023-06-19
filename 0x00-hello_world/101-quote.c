c0bf599619504dbba4ce6f427cf15f4d6b84f32c
veemola <violetachieng848@gmail.com>
2023-06-19 16:22:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line followed by a new line
- * Return: 1 (success)
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
